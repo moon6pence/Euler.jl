8f2ad060f7a0ce835d7d5cd9ca13cfe14e9af48e
khkim <moon6pence@gmail.com>
2014-04-17 16:28:44 +0900

    Fix problem #3, it was wrong

--- 003.jl
+++ 003.jl
@@ -1,5 +1,5 @@
 # https://projecteuler.net/problem=3
-N = 600851475143 
+N = 600851475143
 
 function is_prime(number)
 	for i in 2:ifloor(sqrt(number))
@@ -11,12 +11,20 @@ function is_prime(number)
 	return true
 end
 
-i = ifloor(sqrt(N))
-while i >= 2
-	if N % i == 0 && is_prime(i)
-		println(i)
-		break
-	end
+factor = 2
+largest_factor = factor
+while factor <= N
+	if N % factor == 0 && is_prime(factor)
+		# println(factor)
+		if factor > largest_factor
+			largest_factor = factor
+		end
 
-	i -= 1
+		N = div(N, factor)
+		factor = 2
+	else
+		factor += 1
+	end
 end
+
+println(largest_factor)
